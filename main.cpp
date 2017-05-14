13a63acb553fe08a940c0cd7dad3ec8f93757d8d
Ivan Chernousov <civ-viers@yandex-team.ru>
2017-05-14 22:20:13 +0300

    Hotfix: main.cpp added

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <QtWidgets>
+
+#include "ui/main_window.h"
+
+int main(int argc, char* argv[])
+{
+    QApplication main_app(argc, argv);
+    main_app.setQuitOnLastWindowClosed(false);
+    main_app.setQuitLockEnabled(true);
+
+    ui::MainWindow main_window;
+    main_window.show();
+
+    return main_app.exec();
+}
