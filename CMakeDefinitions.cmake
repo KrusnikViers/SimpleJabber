a434e3a37ddc7c4d71f66711c778a2b4348d78c2
Viers <krusnik.viers@gmail.com>
2017-06-16 03:16:05 +0300

    Build improved and simplified.

---
+++ CMakeDefinitions.cmake
@@ -0,0 +1,34 @@
+# Set common cmake policies.
+cmake_policy(SET CMP0020 NEW)
+cmake_policy(SET CMP0048 NEW)
+cmake_policy(SET CMP0043 NEW)
+
+# Set output directories.
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/out/bin)
+set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/out/bin)
+set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/out/lib)
+foreach(OUTPUTCONFIG ${CMAKE_CONFIGURATION_TYPES})
+    string(TOUPPER ${OUTPUTCONFIG} OUTPUTCONFIG)
+    set(CMAKE_RUNTIME_OUTPUT_DIRECTORY_${OUTPUTCONFIG} ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+    set(CMAKE_LIBRARY_OUTPUT_DIRECTORY_${OUTPUTCONFIG} ${CMAKE_LIBRARY_OUTPUT_DIRECTORY})
+    set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY_${OUTPUTCONFIG} ${CMAKE_ARCHIVE_OUTPUT_DIRECTORY})
+endforeach()
+
+# Adds component from ${name} subdirectory as static library 'sj-${name}'.
+macro(add_component name)
+    add_subdirectory(${name} EXCLUDE_FROM_ALL)
+    add_library(sj-${name} STATIC EXCLUDE_FROM_ALL ${${name}-src})
+    target_compile_options(sj-${name} PUBLIC -Wall -Wextra -Werror)
+    target_include_directories(sj-${name} PUBLIC ${CMAKE_CURRENT_BINARY_DIR} ${CMAKE_CURRENT_SOURCE_DIR})
+endmacro()
+
+# Set option for subdirectory even if other value was cached already.
+macro(set_option name value)
+    set(${name} ${value} CACHE INTERNAL "" FORCE)
+endmacro()
+
+# Links specified Qt5 package to target.
+macro(target_qt5_package target package)
+    find_package(Qt5${package} REQUIRED)
+    qt5_use_modules(${target} ${package})
+endmacro()
